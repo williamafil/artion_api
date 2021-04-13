3a929d9550217648dd5f9fbd619679e00b709654
ToroChu <whileonrails@gmail.com>
2021-04-13 22:29:20 +0800

    add devise i18n

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module ArtionApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = "zh-TW"
+
   end
 end
