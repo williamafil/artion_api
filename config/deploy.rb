6aefa8d5b2b158478085bedcee2087ffa322840a
ToroChu <whileonrails@gmail.com>
2021-05-03 12:40:21 +0800

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,23 +13,23 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
-namespace :deploy do
-  desc 'Restart application'
-    task :restart do
-      on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-  namespace :check do
-    before :linked_files, :set_master_key do
-      on roles(:app), in: :sequence, wait: 10 do
-        unless test("[ -f #{shared_path}/config/master.key ]")
-          upload! 'config/master.key', "#{shared_path}/config/master.key"
-        end
-      end
-    end
-  end
-end
+# namespace :deploy do
+#   desc 'Restart application'
+#     task :restart do
+#       on roles(:app), in: :sequence, wait: 5 do
+#       invoke 'puma:restart'
+#     end
+#   end
+#   namespace :check do
+#     before :linked_files, :set_master_key do
+#       on roles(:app), in: :sequence, wait: 10 do
+#         unless test("[ -f #{shared_path}/config/master.key ]")
+#           upload! 'config/master.key', "#{shared_path}/config/master.key"
+#         end
+#       end
+#     end
+#   end
+# end
 
 namespace :deploy do
   # rails db:seed
@@ -41,12 +41,24 @@ namespace :deploy do
       end
     end
   end
+  # drop db
+  desc 'drop database'
+  task :drop_db do
+    on roles(:app) do
+      within current_path do
+        # execute :bundle, :exec, 'rails', 'db:drop', 'RAILS_ENV=production', 'DISABLE_DATABASE_ENVIRONMENT_CHECK=1'
+        # execute :bundle, :exec, 'rails', 'db:create', 'RAILS_ENV=production'
+        # execute :bundle, :exec, 'rails', 'db:migrate', 'RAILS_ENV=production'
+      end
+    end
+  end
   # 覆蓋原本的 puma 指令
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute '~/.rvm/bin/rvm default do bundle exec --keep-file-descriptors puma'
       # invoke 'puma:restart'
+      invoke 'puma:stop'
+      invoke 'puma:start'
     end
   end
   # 上傳 public底下的東西
