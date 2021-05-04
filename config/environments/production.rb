f1404ba321c214adc9cb7c263fc8c73a5de64215
ToroChu <whileonrails@gmail.com>
2021-05-04 14:42:31 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://artion.balliiballii.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ["https://vigilant-kowalevski-b47053.netlify.app/", "https://artion.balliiballii.com"]
+  config.action_cable.allowed_request_origins = ["https://vigilant-kowalevski-b47053.netlify.app/", "http://artion.balliiballii.com", "https://artion.balliiballii.com"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
