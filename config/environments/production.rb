6cb3297852f94f93e4be20f047030945ea522312
ToroChu <whileonrails@gmail.com>
2021-05-05 06:09:23 +0800

    add default_url_options[:protocol]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   Rails.application.routes.default_url_options[:host] = 'artion.balliiballii.com'
+  Rails.application.routes.default_url_options[:protocol] = 'https'
 end
